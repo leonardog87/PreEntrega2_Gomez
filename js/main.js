7b2e3147c686cb30e3671a44dd6971538bcb783b
Leonardo Gomez <krabatvitae@gmail.com>
2024-09-05 18:03:41 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,38 +1,67 @@
-function saludar() {
-    let ingresoNombre = prompt("Ingresa tu nombre: ");
-    if (ingresoNombre == "") {
-        alert("Debes ingresar tu nombre!")
+//CODERHOUSE
+//Pre entrega N2
+//Leonardo Gomez
+
+const producto = [
+    { id: 1, title: 'Zapatillas', price: 300 },
+    { id: 2, title: 'Botines', price: 350 },
+    { id: 3, title: 'Botas', price: 400 }
+];
+
+const promptFuncion = () => {
+
+    let carrito = [];
+    let IngresoOpcion = parseInt(prompt('Bienvenido a la Web de compra!\n1) Comprar\n0) Salir'));
+
+    if (IngresoOpcion === 0) {
+        console.log('Gracias por tu visita!')
     }
-    else {
-        alert("Bievenida/o a nuestra web" + " " + ingresoNombre);
+
+    else if (IngresoOpcion === 1) {
+        alert('Vamos a comprar!')
+        while (IngresoOpcion !== 0) {
+            let IngresoOpcion = parseInt(prompt('Los productos que elijas iran al carrito,\n cuando termines de comprar sumaremos todo\n y te diremos el total:\n1) Zapatillas $300\n2) Botines $350\n3) Botas $400\n0) Salir'));
+            if (IngresoOpcion === 1) {
+                carrito.push(producto[0])
+                console.log(carrito)
+            }
+
+            else if (IngresoOpcion === 2) {
+                carrito.push(producto[1])
+                console.log(carrito)
+            }
+
+            else if (IngresoOpcion === 3) {
+                carrito.push(producto[2])
+                console.log(carrito)
+            }
+
+            else if (IngresoOpcion === 0) {
+                alert('Compra finalizada')
+                break
+            }
+            else {
+                console.log('temrinado')
+            }
+        }
+
+        const sumaTotal = () => {
+            let suma = [];
+            let titles = [];
+
+            carrito.forEach(element => {
+                let price = element.price;
+                let title = element.title;
+                suma.push(price);
+                titles.push(title);
+                totaltotal = suma.reduce((a, b) => a + b, 0);
+            })
+            console.log('Compraste: ', titles, 'Total: $', totaltotal)
+        };
+        sumaTotal();
     }
 }
 
-// saludar();
+promptFuncion();
 
-function ingresoOpcion() {
-    let ingresoOpcion = parseInt(prompt("Elige una las opciones!\n1) Comprar \n0) Salir"))
-    if (ingresoOpcion === 0) {
-        alert("Hasta luego!")
-    }
-    while (ingresoOpcion !== 0) {
-        ingresoOpcion = parseInt(prompt("Que producto deseas comprar?\n1) Producto1\n2) Producto2\n3) Producto3\n0) Salir"));
-        if (ingresoOpcion === 1) {
-            alert("Seleccionaste el Producto1")
-        }
-        else if (ingresoOpcion === 2) {
-            alert("Seleccionaste el Producto2")
-        }
-        else if (ingresoOpcion === 3) {
-            alert("Seleccionaste el Producto3")
-        }
-        else if (ingresoOpcion === 0) {
-            alert("Hasta luego!")
-        }
-        else {
-            alert("Ingresaste un dato invalido")
-        }
-    }
-}
 
-// ingresoOpcion();
